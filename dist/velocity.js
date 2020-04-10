8eb62b8889a6a6789f899871b1ccadcebc164d7e
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2020-04-10 18:20:58 +0530

    Added composer.

--- dist/velocity.js
+++ dist/velocity.js
@@ -1,5 +1,5 @@
 /**
- * Velocity - v1.0.0-alpha1 - 2020-02-12
+ * Velocity - v1.0.0-alpha1 - 2020-04-10
  * Description: Velocity is a JavaScript library which provide utilities, ui components and MVC framework implementation.
  * License: GPL-3.0-or-later
  * Author: Bhagwat Singh Chouhan
